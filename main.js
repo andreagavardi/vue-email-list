e730bb892657e7a0aa12fc626348a76f74cb4a1e
andreagavardi <andreagavardi89@gmail.com>
2021-05-27 15:58:16 +0200

    lista mail generata

---
+++ main.js
@@ -0,0 +1,33 @@
+ /* Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus:
+Far comparire gli indirizzi email solamente quando sono stati tutti generati. */
+ 
+ 
+  const app = new Vue({ 
+       el: '#app',
+       data:{
+           mailList: [],
+        },
+       methods:{
+           
+        },
+       mounted(){
+
+        //funzione per creare 10 mail casuali tramite l'API
+        function getMailRandom(mailList){
+               for(let i = 0; i < 10; i++){
+                axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then(response =>{
+                        mailList.push(response.data.response)
+                        
+                    })
+               }
+           }
+
+         getMailRandom(this.mailList)
+         
+        
+       } 
+    }) 
